6f2dfa7579bdfca98cffc6c4dda511729a570f2d
FengRuwei <fruwei@163.com>
2016-07-27 23:52:30 +0800

    es

---
+++ src/main/java/com/frw/dev/search/ElasticsSearchTest.java
@@ -0,0 +1,28 @@
+package com.frw.dev.search;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.NodeBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by fruwei on 2016/7/27.
+ */
+public class ElasticsSearchTest {
+
+    static Logger log = LoggerFactory.getLogger("test");
+
+    public static void main(String args[]) {
+
+        TransportClient.Builder build = TransportClient.builder();
+
+        client.a
+
+
+    }
+
+
+}
