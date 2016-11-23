f6624658f095d2ba274700b96c5c358d5d93f75d
fruwei <fruwei12@gmail.com>
2016-11-23 22:57:33 +0800

    随机开关

--- src/main/java/com/frw/net/server/ServerMain.java
+++ src/main/java/com/frw/net/server/ServerMain.java
@@ -1,6 +1,9 @@
 package com.frw.net.server;
 
-import java.io.IOException;
+import com.alibaba.fastjson.JSON;
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,10 +16,37 @@ public class ServerMain {
     static public int port = 1234;
 
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         if (args.length == 1) {
             port = Integer.parseInt(args[0]);
         }
+
+        File f = new File("./switchdata.txt");
+        if (f.exists()) {
+            System.out.println("read data from  switchdata.txt");
+            BufferedReader bf = new BufferedReader(new FileReader(f));
+            String line;
+            StringBuilder sb;
+            int num = 0;
+            Data data = new Data();
+            while ((line = bf.readLine()) != null) {
+                String split[] = line.trim().split(",");
+                long address = Long.parseLong(split[0], 16);
+                if (num == 0) {
+                    data.address = address;
+                    data.name = split[1];
+                } else {
+                    SwitchData switchData = new SwitchData();
+                    switchData.address = address;
+                    switchData.name = split[1];
+                    data.sdata.add(switchData);
+                }
+                num++;
+            }
+            data.num = num - 1;
+            ServerRealDataHandler.strData = JSON.toJSONString(data);
+        }
+
         ServerSocket serverSocket;
         try {
             serverSocket = new ServerSocket(port);
